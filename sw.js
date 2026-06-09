73e20d154041dd4e9ccc0d217d1c9c8f77eba634
PigCHSX <aminors114@hotmail.com>
2026-06-09 20:15:56 +0800

    fix:再次尝试修复

--- sw.js
+++ sw.js
@@ -6,7 +6,10 @@ self.addEventListener('install', event => {
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
-
+// 放行 Supabase 请求，不做拦截
+if (event.request.url.includes('supabase.co')) {
+    return fetch(event.request);
+}
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request))
